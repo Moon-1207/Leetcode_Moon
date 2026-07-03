5b02b8f2c595a56f5a0a16ea11d2d20b49b205b6
HongYue Wang <98253335+Moon-1207@users.noreply.github.com>
2026-07-03 18:37:59 +0800

    Add files via upload

---
+++ 78. 子集.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        path=[]
+
+
+        def backtrack(start_index):
+            res.append(path.copy())
+
+            if start_index>=len(nums):
+                return
+
+            for i in range(start_index,len(nums)):
+                path.append(nums[i])
+                backtrack(i+1)
+                path.pop()
+
+        backtrack(0)
+        return res
